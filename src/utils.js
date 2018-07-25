5abe3c78278233d748a6fec5f7fe64a6450913a5
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-25 17:11:32 +0800

    comment out unused function

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,11 @@ export const getEndpoint = route => {
 		return `${process.env.REACT_APP_BOOKS_API}${route}`;
 	return `http://localhost:3000${route}`;
 };
+
+// export const getDataAndUpdateState = async (endpoint, stateKey) => {
+// 	const response = await fetch(endpoint);
+// 	const data = await response.json();
+// 	this.setState({
+// 		[stateKey]: data
+// 	});
+// };
